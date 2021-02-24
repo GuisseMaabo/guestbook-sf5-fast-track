cc5735699d6951a99a81838cb72e65ebc9f1175c
unknown <cireguisse2015@gmail.com>
2021-02-24 17:40:52 +0100

    Easy admin succesfully done

---
+++ migrations/Version20210224162147.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210224162147 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comment RENAME COLUMN photo_file_name TO photo_filename');
+        $this->addSql('ALTER TABLE conference RENAME COLUMN is_international TO is_internationnal');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE conference RENAME COLUMN is_internationnal TO is_international');
+        $this->addSql('ALTER TABLE comment RENAME COLUMN photo_filename TO photo_file_name');
+    }
+}
