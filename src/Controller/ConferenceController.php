876f5cc0578336f3e98be5dc11300c5afddcd700
unknown <cireguisse2015@gmail.com>
2021-02-26 10:54:59 +0100

    refactoring the controller

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -15,24 +15,45 @@ use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
+
+    /**
+     * You might have noticed that both methods in ConferenceController take
+     * a Twig environment as an argument. Instead of injecting it into each
+     * method, let’s use some constructor injection instead (that makes the list
+     * of arguments shorter and less redundant)
+     */
+
+    private $twig;
+    public function __construct(Environment $twig){
+        $this->twig = $twig;
+    }
     #[Route('/', name: 'homepage')]
-    
-
-
-    public function index (Environment $twig, ConferenceRepository $conferenceRepository)
+    /*public function index (Environment $twig, ConferenceRepository $conferenceRepository)*/
+    public function index(ConferenceRepository $conferenceRepository)
     {
-        return new Response($twig->render('conference/index.html.twig', [
+        // return new Response($twig->render('conference/index.html.twig', [
+            return new Response($this->twig->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
              ]));
     }
-    
-    #[Route('/conference/{id}',  name:"conference")]
-     public function show(Environment $twig, Conference $conference,
-CommentRepository $commentRepository)
- {
- return new Response($twig->render('conference/show.html.twig', [
- 'conference' => $conference,
- 'comments' => $commentRepository->findBy(['conference'=>$conference], ['createdAt' => 'DESC']),]));
- }
 
+    #[Route('/conference/{id}',  name:"conference")]
+     /*public function show(Environment $twig, Conference $conference,
+CommentRepository $commentRepository)*/
+
+//public function show (Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository)
+public function show(Request $request, Conference $conference, CommentRepository $commentRepository){
+    
+    $offset = max(0, $request->query->getInt('offset',0));
+    $paginator =  $commentRepository->getCommentPaginator($conference,$offset);
+
+   // return new Response($twig->render('conference/show.html.twig', [
+    return new Response($this->twig->render('conference/show.html.twig', [   
+    'conference' => $conference,
+    //'comments' => $commentRepository->findBy(['conference'=>$conference], ['createdAt' => 'DESC']),]));
+    'comments' => $paginator,
+    'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE, 
+    'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+ ]));
+ }
 }
